37b194c09b053f3f374d1c03d2329f25cd6f060e
Denis Perchenko <plachenko@gmail.com>
2025-01-29 00:16:53 -0500

    Updates

---
+++ _app/immutable/chunks/BGunUCBO.js
@@ -0,0 +1,1 @@
+import{e}from"./CaFSpNbJ.js";e();
---
+++ _app/immutable/nodes/2.BPesEmP0.js
@@ -0,0 +1,1 @@
+import{a as o,t as a}from"../chunks/DQMpbO-H.js";import"../chunks/BGunUCBO.js";import{n as p}from"../chunks/CaFSpNbJ.js";var r=a('<h1>Welcome to SvelteKit</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>',1);function s(t){var e=r();p(2),o(t,e)}export{s as component};
